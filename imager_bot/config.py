401b0c0e64be7eb7f118c5439b19aaedf93510b5
Mikhail Drozdov <deathlokmike@gmail.com>
2024-05-06 22:44:08 +0300

    🚧Added patterns

--- imager_bot/config.py
+++ imager_bot/config.py
@@ -3,6 +3,12 @@ from typing import Literal
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class Patterns:
+    CHOOSE_LANG_CALLBACK = "choose_lang_btn"
+    LANG_CALLBACK = "lang_"
+    ADD_BOT_TO_GROUP_CALLBACK = "add_bot_to_group_btn"
+
+
 class Settings(BaseSettings):
     MODE: Literal["DEV", "TEST", "PROD"]
     LOG_LEVEL: str
@@ -25,11 +31,13 @@ class Settings(BaseSettings):
     TEST_DB_PASS: str
     TEST_DB_NAME: str
 
+    PATTERNS = Patterns()
+
     @property
     def get_test_database_url(self) -> str:
         return f"{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", ignored_types=(Patterns,))
 
 
 settings = Settings()
